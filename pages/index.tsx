6ad981daae912e4a2e5498805cbaa797941bf3e6
clfhhc <david.chen@minted.com>
2019-12-09 15:24:57 -0800

    adding eslint and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
 
-const index: NextPage = () => (
-  <div>Welcome to here</div>
-);
+const index: NextPage = () => <div>Welcome to here</div>;
 
 export default index;
